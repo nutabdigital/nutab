85b55d7d19934304f7253b478d2b0d68526a51da
nutab <nutabdigital@gmail.com>
2024-11-17 15:31:14 -0700

    update package.json to export script

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-// next.config.js
 const nextConfig = {
     output: 'export',
     // Any other configurations you have
-  };  
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
+  

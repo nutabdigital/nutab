a89289f178b3d9bbe6bf182ac8244e0d93987fa2
nutab <nutabdigital@gmail.com>
2024-11-20 17:28:09 -0700

    removed comments from config file

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // distDir: 'dist',
-  // basePath: '/nutab', // Replace 'repository-name' with the actual name of your GitHub repository
-  // assetPrefix: '/nutab', // Ensures assets like images are correctly loaded
 };
 
 export default nextConfig;
